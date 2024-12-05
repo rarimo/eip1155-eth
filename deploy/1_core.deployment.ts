72a263a17511bef926f6e5b46da05f1e111817ad
Kyryl R <kyryl.ryabov@gmail.com>
2024-12-05 15:55:37 +0200

    Added deployment script

---
+++ deploy/1_core.deployment.ts
@@ -0,0 +1,21 @@
+import { ethers } from "ethers";
+
+import { Deployer, Reporter } from "@solarity/hardhat-migrate";
+
+import { ERC1155ETH__factory, ERC1967Proxy__factory, QueryIdentityProofVerifier__factory } from "@ethers-v6";
+
+const MAGIC_TOKEN_ID = BigInt(ethers.id("MAGIC"));
+const STATE_CONTRACT_ADDRESS = "0x10f370A6d8782E0e0E85ba948be6DA2465Aab4E2";
+
+export = async (deployer: Deployer) => {
+  let core = await deployer.deploy(ERC1155ETH__factory);
+  await deployer.deploy(ERC1967Proxy__factory, [await core.getAddress(), "0x"], { name: "ERC1155ETHProxy" });
+
+  core = await deployer.deployed(ERC1155ETH__factory, "ERC1155ETHProxy");
+
+  const verifier = await deployer.deploy(QueryIdentityProofVerifier__factory);
+
+  await core.__ERC1155ETH_init(MAGIC_TOKEN_ID, await verifier.getAddress(), STATE_CONTRACT_ADDRESS);
+
+  Reporter.reportContracts(["ERC1155ETH", await core.getAddress()]);
+};
--- test/circuit.init.test.ts
+++ test/circuit.init.test.ts
@@ -3,7 +3,5 @@ import { zkit } from "hardhat";
 describe("Circuit init test", () => {
   it("test", async () => {
     const circuit = await zkit.getCircuit("queryIdentity");
-
-    console.log(await circuit.generateProof({}));
   });
 });
